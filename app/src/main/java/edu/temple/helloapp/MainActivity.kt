6531e2c078e3831c110d05a503f22eb75e74708a
Mohammed Karim <makarim913@temple.edu>
2025-09-04 12:21:54 -0400

    Added Botton with text

--- app/src/main/java/edu/temple/helloapp/MainActivity.kt
+++ app/src/main/java/edu/temple/helloapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package edu.temple.helloapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +13,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        findViewById<Button>(R.id.button).setOnClickListener {
+            findViewById<TextView>(R.id.textView).text = "you Clicked me!"
+        }
     }
 }
